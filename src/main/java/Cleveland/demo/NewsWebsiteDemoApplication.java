58e5e49e9619d8d30eafded4e51fd8912efdfe46
Stephen Cleveland <cosmokai2000@gmail.com>
2023-10-11 17:46:50 -0500

    initial project commit

---
+++ src/main/java/Cleveland/demo/NewsWebsiteDemoApplication.java
@@ -0,0 +1,13 @@
+package Cleveland.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsWebsiteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsWebsiteDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cleveland/demo/NewsWebsiteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Cleveland.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWebsiteDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
